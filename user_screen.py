73d5d39f5a89c396b88c532c062259c0e8f04661
LSanchez08 <LUIS.ALBERTO.SANCHEZ.RIOS@LIVE.COM.MX>
2020-11-09 20:03:14 -0600

    User

---
+++ user_screen.py
@@ -0,0 +1,60 @@
+import pygame
+import const, title_screen, sys
+
+def username():
+    pygame.init()
+    pygame.display.set_caption("Alien Strike: Retribution Day")
+    width = 1200
+    height = 800
+    size = (width, height)
+    screen = pygame.display.set_mode(size)
+    text = ""
+    x = 0
+    y = 0
+
+    #Global values
+    background = pygame.image.load("assets/visual/menu.png")
+    background = pygame.transform.scale(background, size)
+    settings = pygame.image.load("assets/visual/settings.png")
+
+    settings = pygame.transform.scale(settings, (80, 80))
+    settingsRect = settings.get_rect()
+    settingsRect[0], settingsRect[1] = 20, int(height * 0.85)
+
+    clock = pygame.time.Clock()
+    fps = 60
+    font = pygame.font.Font("fonts/ufonts.com_windpower.ttf", 50)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    return text
+                    text = ''
+                elif event.key == pygame.K_BACKSPACE:
+                    text = text[:-1]
+                else:
+                    text += event.unicode
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                x, y = pygame.mouse.get_pos()
+
+
+
+        screen.fill((const.BLACK))
+        txt_surface = font.render(text, True, const.RED)
+
+
+        screen.blit(txt_surface, (x, y))
+
+        pygame.display.flip()
+        clock.tick(30)
+
+
+    pygame.quit()
+
+
+Name = username()
+
+print(Name)
