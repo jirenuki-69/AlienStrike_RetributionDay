beb2e1a52a9da2bacebfa13920743cf58717c327
jirenuki-69 <mfuentesmodelo@gmail.com>
2020-12-02 17:31:49 -0600

    fqfq

---
+++ clases/Sound.py
@@ -0,0 +1,41 @@
+import pygame, const
+
+class Sound():
+    def __init__(self):
+        pass
+
+    def player_shoot(self):
+        # self.sound = pygame.mixer.Sound("assets/music/SFX/player_shoot.wav")
+        # self.sound.set_volume(.1)
+        # self.sound.play()
+        pass
+
+    def alien_shoot(self):
+        # self.sound = pygame.mixer.Sound("assets/music/SFX/alien_shoot.wav")
+        # self.sound.set_volume(.1)
+        # self.sound.play()
+        pass
+
+    def alien_explosion(self):
+        # self.sound = pygame.mixer.Sound("assets/music/SFX/dead_boom.wav")
+        # self.sound.set_volume(.1)
+        # self.sound.play()
+        pass
+
+    def player_explosion(self):
+        # self.sound = pygame.mixer.Sound("assets/music/SFX/dead_boom.wav")
+        # self.sound.set_volume(.03)
+        # self.sound.play()
+        pass
+
+    def boss_explosion(self):
+        # self.sound = pygame.mixer.Sound("assets/music/SFX/dead_boom.wav")
+        # self.sound.set_volume(.2)
+        # self.sound.play()
+        pass
+
+    def get_ready(self):
+        # self.sound = pygame.mixer.Sound("assets/music/SFX/get_ready.wav")
+        # self.sound.set_volume(.2)
+        # self.sound.play()
+        pass
